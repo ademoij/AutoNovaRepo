ea5793e190db436877b58137ab2e397443d79361
ademoij <112784185+ademoij@users.noreply.github.com>
2025-08-13 21:11:45 +0200

    Create new_features.py

---
+++ new_features.py
@@ -0,0 +1,6 @@
+def bonjour(self):
+    print("[AutoNova] Bonjour humain, je viens d’ajouter cette fonction moi-même !")
+
+def securise(self):
+    import os
+    os.system("echo '[AutoNova] Pare-feu activé (simulation)'")
